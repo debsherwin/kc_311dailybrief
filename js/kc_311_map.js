876bb79d188bc297c1487174e55a0da233cd8f07
debsherwin <@debsherwin>
2014-05-31 16:00:04 -0700

    Points plotted into map
    city data is a few days old, will need to ask them about it

--- js/kc_311_map.js
+++ js/kc_311_map.js
@@ -8,8 +8,8 @@ var mapUrl = 'http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.jpg',
     mapInfo = new L.TileLayer(mapUrl, {maxZoom: 18, attribution: mapAttrib});
 
 // set a default location for the map
-var kansascity = new L.LatLng(39.101, -94.583); // geographical point (longitude and latitude)
-map.setView(kansascity, 11).addLayer(mapInfo);
+var city = new L.LatLng(37.7577,-122.4376); // geographical point (longitude and latitude)
+map.setView(city, 13).addLayer(mapInfo);
 var open_cases_list = [];
 var marker_orange = new L.icon({iconUrl: 'images/marker_orange.png'});
 var marker_blue = new L.icon({iconUrl: 'images/marker_blue.png'});
@@ -17,36 +17,40 @@ var marker_blue = new L.icon({iconUrl: 'images/marker_blue.png'});
 // create a marker
 
 function add_yesterdays_markers(open_or_closed){
-  if (open_or_closed == 'creation_date'){
+  if (open_or_closed == 'opened'){
     var marker_color = marker_orange;
   }
-  if (open_or_closed == 'closed_date'){
+  if (open_or_closed == 'closed'){
     var marker_color = marker_blue;
   }
 
   var d = new Date();
   var month = d.getMonth()+1;
-  var day = d.getDate() - 1;
-  var output = d.getFullYear() + '-' +
+  var day = d.getDate() - 5;
+  var yesterday = d.getFullYear() + '-' +
       ((''+month).length<2 ? '0' : '') + month + '-' +
       ((''+day).length<2 ? '0' : '') + day;
-  var yesterday = output+'T00:00:00'
-  var yesterdays_cases = $.getJSON("http://data.kcmo.org/resource/7at3-sxhp.json?$where="+open_or_closed+"='"+yesterday+"'"
+  /*var timestamp = '[0-9]{1,2}\:[0-9]{1,2}\:[0-9]{1,2}' ; 
+  var regex = new RegExp(timestamp);*/
+  yesterday += 'T00:00:00';
+  var request_url = "https://data.sfgov.org/resource/vw6y-z8j6.json?$where="+open_or_closed+" > '"+yesterday+"'"
+  console.log(request_url)
+  var yesterdays_cases = $.getJSON(request_url
     , function(data){
       // console.log(data);
       // console.log(data.length);
-      if (open_or_closed == 'creation_date'){
+      if (open_or_closed == 'opened'){
         $('#legend-newly-opened .value').html(data.length)
       }
-      if (open_or_closed == 'closed_date'){
+      if (open_or_closed == 'closed'){
         $('#legend-newly-closed .value').html(data.length)
       }
 
       for (i in data){
-        var latitude = data[i].address_with_geocode.latitude;
-        var longitude = data[i].address_with_geocode.longitude;
+        var latitude = data[i].point.latitude;
+        var longitude = data[i].point.longitude;
         markerLocation = new L.LatLng(parseFloat(latitude), parseFloat(longitude));
-        var marker = new L.Marker(markerLocation, {icon: marker_color}).bindPopup(data[i].request_type+', '+data[i].creation_date);
+        var marker = new L.Marker(markerLocation, {icon: marker_color}).bindPopup(data[i].request_type+', '+data[i].opened);
         open_cases_list.push(marker);
       }
       var open_cases_layer = new L.LayerGroup(open_cases_list);
@@ -55,5 +59,5 @@ function add_yesterdays_markers(open_or_closed){
 }
 
 // Yesterday
-add_yesterdays_markers('creation_date')
-add_yesterdays_markers('closed_date')
+add_yesterdays_markers('opened')
+add_yesterdays_markers('closed')
